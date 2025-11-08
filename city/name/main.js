a319dfced1415734d482cf01c3941b232f457d5c
Jimmxyz <Jimmynoux@protonmail.com>
2025-11-08 21:03:23 +0100

    small update

--- city/name/main.js
+++ city/name/main.js
@@ -32,9 +32,10 @@ if (!isNaN(0) && 0 >= 0 && 0 <= document.getElementById('prgs').max) {
     document.getElementById('prgs').value = 0;
 }
 
+document.getElementById("back").disabled = false;
+
 //init
-let playable = true
-let score = []
+let loseStrick = 0;
 
 function back() {
   window.location.replace("../../");
@@ -72,15 +73,27 @@ function textVerified(){
           document.getElementById('errorText').style.color = "rgba(109, 241, 118, 1)"
         }
         document.getElementById('errorText').innerHTML = "Tu as trouvée : " + listCity[i].name
+        document.getElementById('intArea').style.visibility = 'hidden'
+        lastInt = []
+        indexInt++;
+        loseStrick = 0;
         foundedCity.push(listCity[i].name);
         if (!isNaN(foundedCity.length) && foundedCity.length >= 0 && foundedCity.length <= document.getElementById('prgs').max) {
           document.getElementById('prgs').value = foundedCity.length;
+        }
+        if(document.getElementById('prgs').max == document.getElementById('prgs').value){
+          document.getElementById('back').disabled = true;
+          document.getElementById('continue').disabled = true;
         }
         return
       }
     }
     document.getElementById('errorText').style.color = "rgb(241, 109, 109)"
     document.getElementById('errorText').innerHTML = "Nom invalide"
+    loseStrick++;
+    if(loseStrick % 3 == 0){
+      intmaker();
+    }
     return
 }
 
@@ -106,4 +119,77 @@ function checkDiff(str1,str2){
   }
 
   return dp[m][n];
+}
+
+let intCityList = shuffle(listCityDefault);
+let indexInt = 0
+
+
+function intmaker(){
+  let choosenInt = intCityList[indexInt]; 
+  while (searchFounded(choosenInt) == true){
+    if(indexInt >= intCityList.length){
+      document.getElementById('intArea').style.visibility = 'hidden';
+      return
+    }
+    choosenInt = intCityList[indexInt]
+  }
+  document.getElementById('intField').innerText = intDisplay(choosenInt.name);
+  document.getElementById('intArea').style.visibility = 'visible';
+}
+
+function shuffle(list) {
+  const arr = [...list]; 
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+function searchFounded(a){
+  for(let i = 0;i < foundedCity.length; i++){
+    if(a == foundedCity[i]){
+      return true
+    }
+  }
+  return false
+}
+
+let lastInt = [];
+
+function intDisplay(str){
+  let divised = str.split("");
+  //init
+  if(lastInt.length == 0){
+    for(let i = 0; i < divised.length; i++){
+      lastInt.push("_")
+    }
+  }
+  //continue
+  let alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  let rng =  alpha[Math.floor(Math.random() * alpha.length)];
+  let noEternal = 50
+  while (!str.normalize("NFD").toLowerCase().includes(rng) || lastInt.join("").normalize("NFD").toLowerCase().includes(rng)){
+    rng =  alpha[Math.floor(Math.random() * alpha.length)];
+    noEternal--
+    if(noEternal <= 0){
+      break
+    }
+  }
+
+  let checkDifAfter = lastInt;
+  for(let i = 0; i < divised.length; i++){
+    console.log(rng)  
+    if(checkDiff(divised[i],rng) == 0){
+        lastInt[i] = divised[i];
+      }
+  }
+
+
+  let returnedInt = ""
+  for(let i = 0; i < lastInt.length; i++){
+      returnedInt += lastInt[i] + " "
+  }
+  return returnedInt
 }
